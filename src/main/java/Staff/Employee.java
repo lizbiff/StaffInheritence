2943cbce56cd424185aa086d96183b2da354583c
Sarah Grant <sarah@ghijk.co.uk>
2019-11-11 18:43:04 +0000

    set up employee and manager classes

---
+++ src/main/java/Staff/Employee.java
@@ -0,0 +1,36 @@
+package Staff;
+
+public abstract class Employee {
+
+    private String name;
+    private String ni;
+    private int salary;
+
+    public Employee(String name, String ni, int salary){
+        this.name = name;
+        this.ni = ni;
+        this.salary = salary;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNi() {
+        return ni;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public abstract double raiseSalary();
+
+    public int payBonus(){
+//        bonusAmount = (this.salary * 0.01);
+        return (this.salary * 0.01);
+    }
+
+
+}
---
+++ src/main/java/Staff/Techstaff/DataBaseAdmin.java
@@ -0,0 +1,4 @@
+package Staff.Techstaff;
+
+public class DataBaseAdmin {
+}
